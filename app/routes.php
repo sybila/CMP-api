0f6d140dc69b602e19028b07d75f676a73163cc1
lachtan <xhrabec@gmail.com>
2018-04-18 16:52:11 +0200

    put/delete shouldn't allow multiple IDs!

--- app/routes.php
+++ app/routes.php
@@ -8,7 +8,7 @@ use Slim\Http\Response;
 
 return function(App $app)
 {
-	$idRegex = '{id:(?:\\d,?)+}';
+	$multiIdRegex = '{id:(?:\\d,?)+}';
 
 	// main
 	$app->get('/', function (Request $request, Response $response, Helpers\ArgumentParser $args)
@@ -21,25 +21,25 @@ return function(App $app)
 
 	// entities
 	$app->get('/entities', Ctl\EntityController::class . ':read');
-	$app->get('/entities/' . $idRegex, Ctl\EntityController::class . ':readIdentified');
+	$app->get('/entities/' . $multiIdRegex, Ctl\EntityController::class . ':readIdentified');
 	$app->post('/entities', Ctl\EntityController::class . ':add');
-	$app->put('/entities/' . $idRegex, Ctl\EntityController::class . ':edit');
-	$app->post('/entities/' . $idRegex . '/status', Ctl\EntityController::class . ':editStatus');
-	$app->delete('/entities/' . $idRegex, Ctl\EntityController::class . ':delete');
+	$app->put('/entities/{id:\\d+}', Ctl\EntityController::class . ':edit');
+	$app->post('/entities/{id:\\d+}/status', Ctl\EntityController::class . ':editStatus');
+	$app->delete('/entities/{id:\\d+}', Ctl\EntityController::class . ':delete');
 	$app->get('/entities/{code}', Ctl\EntityController::class . ':readCode');
 
 	// rules
 	$app->get('/rules', Ctl\RuleController::class . ':read');
-	$app->get('/rules/' . $idRegex, Ctl\RuleController::class . ':readIdentified');
+	$app->get('/rules/' . $multiIdRegex, Ctl\RuleController::class . ':readIdentified');
 	//$app->post('/rules', C\RuleController::class . ':add');
-	//$app->put('/rules/' . $idRegex, C\RuleController::class . ':edit');
-	//$app->delete('/rules/' . $idRegex, C\RuleController::class . ':delete');
+	//$app->put('/rules/{id:\\d+}', C\RuleController::class . ':edit');
+	//$app->delete('/rules/{id:\\d+}', C\RuleController::class . ':delete');
 
 	// organisms
-	$app->get('/organisms/' . $idRegex, Ctl\OrganismController::class . ':readIdentified');
+	$app->get('/organisms/' . $multiIdRegex, Ctl\OrganismController::class . ':readIdentified');
 	$app->get('/organisms', Ctl\OrganismController::class . ':read');
 
 	// Classifications
-	$app->get('/classifications/' . $idRegex, Ctl\ClassificationController::class . ':readIdentified');
+	$app->get('/classifications/' . $multiIdRegex, Ctl\ClassificationController::class . ':readIdentified');
 	$app->get('/classifications[/{type}]', Ctl\ClassificationController::class . ':read');
 };
